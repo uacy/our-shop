d097f477f86d1d9433fdc641d4d0e351be2b026f
Student User <student@cs.pub.ro>
2014-07-03 15:58:03 +0300

    First commit

---
+++ src/com/example/firstpage/ItemsList.java
@@ -0,0 +1,97 @@
+package com.example.firstpage;
+
+import java.util.ArrayList;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
+import org.apache.http.*;
+import org.json.*;
+
+
+public class ItemsList extends ListActivity {
+	 ArrayList<String> nume = new ArrayList<String>();
+	 ListView lista;
+	 ArrayAdapter<String> adapter;
+	 String rez;
+	    
+	 protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.items);
+	        lista = (ListView) findViewById(android.R.id.list);
+	        
+	       
+	        
+	        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nume);
+	        setListAdapter(adapter);
+	       
+	     RequestParams params = new RequestParams();
+	        params.put("api_auth", "VECeKU2puHatudreb7A3");
+	        params.put("trigger", "user_profile");
+	        params.put("id_user", "35");
+	        params.put("session", "61ff95571aa3db550df827f0cae10c938cd31fd2");
+
+	        AsyncHttpClient client = new AsyncHttpClient();
+	        
+	        
+	        client.get("http://10.0.2.2:5000/api/list", new JsonHttpResponseHandler() {
+	            @Override
+	            public void onStart() {
+	                // called before request is started
+	            }
+	            @Override
+	            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+	                // called when response HTTP status is "200 OK"
+	                rez = new String(response.toString());
+	                
+	               
+	                String []tok = rez.split("\"");
+	                int i = 0;
+	                for(String token:tok)
+	                {
+	                	char tkst = token.charAt(0);
+	                	if(!(tkst >= 'a' && tkst <='z')&&!(tkst >= 'A' && tkst <='Z')) continue;
+	                	if(token.equals("name")) continue;
+	                	nume.add(token);
+	                	adapter.notifyDataSetChanged();
+	                	System.out.println(nume.get(i++));
+	                }
+	                
+	                System.out.println("Succes");
+	            }
+	            @Override
+	            public void onFailure(int statusCode, Header[] headers, java.lang.Throwable throwable, JSONObject errorResponse) {
+	                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+	                System.out.println(errorResponse.toString());
+	            }
+	            @Override
+	            public void onRetry(int retryNo) {
+	                // called when request is retried
+	            }
+	        });
+	       lista.setOnItemClickListener(new OnItemClickListener() {
+	            @Override
+	            public void onItemClick(AdapterView<?> adaptor, View arg1, int position,
+	                    long id) {
+	            		Intent intent = new Intent(ItemsList.this, ShoppingCart.class);
+	                	startActivity(intent);
+	            		
+	            }
+	       }); 
+	      /* System.out.println("Printing nume");System.out.println(nume.get(0));
+	       System.out.println("lungimea: " + nume.size());*/
+	       
+		}
+	 
+
+}
---
+++ src/com/example/firstpage/ShoppingCart.java
@@ -0,0 +1,112 @@
+package com.example.firstpage;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class ShoppingCart extends ListActivity {
+	Button btnbuy;
+	Button btnadd;
+	final Context context = this;
+    ArrayList<String> nume;
+    String items;
+    ListView lista;
+    ArrayAdapter<String> adapter;
+	Bundle b = new Bundle();
+
+
+    
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cart);
+        lista = (ListView) findViewById(android.R.id.list);
+
+        nume = new ArrayList<String>();
+        
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nume);
+        setListAdapter(adapter);
+
+        lista.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adaptor, View arg1, int position,
+                    long id) {
+                Toast.makeText(ShoppingCart.this, "Click "+ adaptor.getItemAtPosition(position)+ "?", Toast.LENGTH_LONG).show()    ;
+            }
+            
+
+        });
+        
+        lista.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                    int pos, long id) {
+				nume.remove(pos);
+				adapter.notifyDataSetChanged();
+
+                return true;
+            }
+        });
+        btnbuy = (Button)findViewById(R.id.buy);
+        btnbuy.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+						context);
+		 
+					// set title
+					alertDialogBuilder.setTitle("Are you sure?");
+		 
+					// set dialog message
+					alertDialogBuilder
+						.setMessage("Click yes to continue!")
+						.setCancelable(false)
+						.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,int id) {
+								ShoppingCart.this.finish();
+							}
+						  })
+						.setNegativeButton("No",new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,int id) {
+								dialog.cancel();
+							}
+						});
+		 
+						AlertDialog alertDialog = alertDialogBuilder.create();
+						alertDialog.show();
+					}
+		});	
+        btnadd = (Button)findViewById(R.id.add);	
+        btnadd.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+            	Intent intent2 = new Intent(ShoppingCart.this, ItemsList.class);
+            	startActivity(intent2);
+  
+				
+			}
+		});
+
+
+			}  
+	
+
+	}
+
